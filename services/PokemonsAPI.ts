afda8be4740e25335def69f281dc2817596716a8
bencun <axa993@gmail.com>
2021-12-19 18:07:43 +0100

    Lint and format.

--- services/PokemonsAPI.ts
+++ services/PokemonsAPI.ts
@@ -1,4 +1,4 @@
-import _Pokedex, {Pokemon as PokedexPokemon} from 'pokedex-promise-v2';
+import _Pokedex, { Pokemon as PokedexPokemon } from 'pokedex-promise-v2';
 
 export type Pokemon = {
   name: string;
@@ -7,27 +7,49 @@ export type Pokemon = {
   weight: number;
   sprites: {
     front_default: string | null;
-  }
-}
+  };
+};
 
 const Pokedex = new _Pokedex();
 
 export const PokemonsAPI = {
   async getAll(offset: number = 0): Promise<Pokemon[]> {
-    const pokemonList = await Pokedex.getPokemonsList({offset, limit: 10});
+    const pokemonList = await Pokedex.getPokemonsList({ offset, limit: 10 });
     const pokemons: Pokemon[] = [];
     for (const p of pokemonList.results) {
-      const {name, base_experience, height, weight, abilities, sprites: {front_default} } =
-        await Pokedex.getPokemonByName(p.name) as PokedexPokemon;
+      const {
+        name,
+        base_experience,
+        height,
+        weight,
+        abilities,
+        sprites: { front_default },
+      } = (await Pokedex.getPokemonByName(p.name)) as PokedexPokemon;
       // grab only the data we need
       pokemons.push({
-        name, base_experience, height, weight, sprites: {front_default}
+        name,
+        base_experience,
+        height,
+        weight,
+        sprites: { front_default },
       });
     }
     return pokemons;
   },
   async getOne(pokemonName: string): Promise<Pokemon> {
-    const {name, base_experience, height, weight, sprites: {front_default}} = await Pokedex.getPokemonByName(pokemonName) as PokedexPokemon;
-    return {name, base_experience, height, weight, sprites: {front_default}};
-  }
+    const {
+      name,
+      base_experience,
+      height,
+      weight,
+      sprites: { front_default },
+    } = (await Pokedex.getPokemonByName(pokemonName)) as PokedexPokemon;
+    return {
+      name,
+      base_experience,
+      height,
+      weight,
+      sprites: { front_default },
+    };
+  },
 };
